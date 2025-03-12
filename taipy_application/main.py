a4e495369d38f88461fcb18b8aa407e40788fc59
gabrielbdornass <99965186+gabrielbdornass@users.noreply.github.com>
2025-03-12 22:02:00 +0000

    Create first dashboard
    See https://docs.taipy.io/en/latest/tutorials/articles/sales_dashboard/step_01/step_01/#__tabbed_1_1

--- taipy_application/main.py
+++ taipy_application/main.py
@@ -1,25 +1,47 @@
-# Copyright 2021-2024 Avaiga Private Limited
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
 from taipy.gui import Gui
+import taipy.gui.builder as tgb
+import pandas as pd
+
+data = pd.read_csv("data.csv")
+chart_data = (
+    data.groupby("State")["Sales"]
+    .sum()
+    .sort_values(ascending=False)
+    .head(10)
+    .reset_index()
+)
+
+categories = list(data["Category"].unique())
+selected_category = "Furniture"
+
+layout = {"yaxis": {"title": "Revenue (USD)"}, "title": "Sales by State"}
 
 
-page = """
-<center>
-<|navbar|lov={[("home", "Homepage")]}|>
-</center>
+def change_category(state):
+    state.data = data[data["Category"] == state.selected_category]
+    state.chart_data = (
+        state.data.groupby("State")["Sales"]
+        .sum()
+        .sort_values(ascending=False)
+        .head(10)
+        .reset_index()
+    )
+    state.layout = {
+        "yaxis": {"title": "Revenue (USD)"},
+        "title": f"Sales by State for {state.selected_category}",
+    }
 
-"""
 
+with tgb.Page() as page:
+    tgb.selector(value="{selected_category}", lov=categories, on_change=change_category)
+    tgb.chart(
+        data="{chart_data}",
+        x="State",
+        y="Sales",
+        type="bar",
+        layout="{layout}",
+    )
+    tgb.html("br")
+    tgb.table(data="{data}")
 
-if __name__ == "__main__":
-
-    gui = Gui(page=page)
-    gui.run(title="Taipy Application")
+Gui(page=page).run(title="Sales", dark_mode=False)
